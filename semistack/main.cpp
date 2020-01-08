8c4ad3d4c12c75899dc8d1d9863190bf8b8cdb9b
Zeke Medley <zekemedley@gmail.com>
2020-01-07 20:07:07 -0800

    Initial Commit

---
+++ semistack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  semistack
+//
+//  Created by Zeke Medley on 1/7/20.
+//  Copyright © 2020 Zeke Medley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
